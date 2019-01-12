9b94489ff600ef23ecf6421bba80b8aeae962279
Daniel Voyce <dan@voyced.com>
2019-01-12 08:03:00 +0700

    WIP

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,8 @@
-import { BASE_URL, NOW_PLAYING_PATH } from './api.constants';
+import { BASE_URL, NOW_PLAYING_PATH } from "./api.constants";
 
 const fetchMovies = () => {
   return fetch(
-    `${BASE_URL}${NOW_PLAYING_PATH}?api_key=09991eb2985bce3334ee07e936171ea8`
+    `${BASE_URL}/discover/movie?api_key=09991eb2985bce3334ee07e936171ea8&sort_by=popularity.asc`
   );
 };
 
